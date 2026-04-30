082045fb7e82485f604ff0c99cfe9896095903b7
Fzand <farzanzand86@gmail.com>
2026-04-30 13:49:47 +0200

    WakeUp

--- Assets/Scripts/Managers/WorldManager.cs
+++ Assets/Scripts/Managers/WorldManager.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class WorldManager : Singleton<WorldManager>
 {
+    public PlayableDirector WakeUpDirector;
 
+    public void Start()
+    {
+        WakeUpReset();
+    }
     
-    
+    public void WakeUpReset()
+    {
+        WakeUpDirector.Play();
+    }
 }
