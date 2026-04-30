ddbb147fe99a7838846c8f092c47601f8441c71e
Fzand <farzanzand86@gmail.com>
2026-04-30 14:49:23 +0200

    Wake up, Audiomanager, ScreenManager

--- Assets/Scripts/Managers/ScreenManager.cs
+++ Assets/Scripts/Managers/ScreenManager.cs
@@ -1,16 +1,62 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScreenManager : MonoBehaviour
+public class ScreenManager : Singleton<ScreenManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Image fadeFullscreenImage;
+
+    private Coroutine fadeRoutine;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        SetAlpha(0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// Starts invisible, waits holdDuration, then fades to fully visible over fadeDuration.
+    public void FadeIn(float fadeDuration, float holdDuration = 0f)
+        => RunFade(from: 0f, to: 1f, fadeDuration, holdDuration);
+
+    /// Starts fully visible, waits holdDuration, then fades to invisible over fadeDuration.
+    public void FadeOut(float fadeDuration, float holdDuration = 0f)
+        => RunFade(from: 1f, to: 0f, fadeDuration, holdDuration);
+
+    void RunFade(float from, float to, float fadeDuration, float holdDuration)
     {
-        
+        if (fadeRoutine != null) StopCoroutine(fadeRoutine);
+        fadeRoutine = StartCoroutine(FadeRoutine(from, to, fadeDuration, holdDuration));
+    }
+
+    IEnumerator FadeRoutine(float from, float to, float fadeDuration, float holdDuration)
+    {
+        SetAlpha(from);
+
+        if (holdDuration > 0f)
+            yield return new WaitForSecondsRealtime(holdDuration);
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.unscaledDeltaTime;
+            SetAlpha(Mathf.Lerp(from, to, Mathf.Clamp01(elapsed / fadeDuration)));
+            yield return null;
+        }
+
+        SetAlpha(to);
+        fadeRoutine = null;
+    }
+
+    void SetAlpha(float alpha)
+    {
+        if (fadeFullscreenImage == null) return;
+        bool visible = alpha > 0f;
+        fadeFullscreenImage.gameObject.SetActive(visible);
+        if (visible)
+        {
+            var c = fadeFullscreenImage.color;
+            c.a = alpha;
+            fadeFullscreenImage.color = c;
+        }
     }
 }
--- Assets/Scripts/Managers/WorldManager.cs
+++ Assets/Scripts/Managers/WorldManager.cs
@@ -1,17 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class WorldManager : Singleton<WorldManager>
 {
     public PlayableDirector WakeUpDirector;
+    public Transform startPositionRoom;
 
     public void Start()
     {
         WakeUpReset();
     }
-    
+
     public void WakeUpReset()
     {
+        Debug.Log("Wake up reset");
+        ScreenManager.Instance.FadeOut(1.5f, 1.5f);
+        PlayerManager.Instance.roomPlayer.transform.SetPositionAndRotation(startPositionRoom.position, startPositionRoom.rotation);
+
+        MFPC.PlayerController.instance.enabled = false;
+        WakeUpDirector.stopped += OnWakeUpComplete;
         WakeUpDirector.Play();
+        StartCoroutine(PlayGroundhogAudio());
+    }
+
+    void OnWakeUpComplete(PlayableDirector _)
+    {
+        WakeUpDirector.stopped -= OnWakeUpComplete;
+        MFPC.PlayerController.instance.enabled = true;
+    }
+
+    IEnumerator PlayGroundhogAudio()
+    {
+        yield return new WaitForSeconds(1f);
+        var src = AudioManager.Instance.PlaySFX2D("groundhog");
+        yield return new WaitForSeconds(10f);
+        if (src != null) src.Stop();
     }
 }
