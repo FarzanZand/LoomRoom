b237705adb2f251f23bbc87972ee529c37afee03
Fzand <farzanzand86@gmail.com>
2026-04-30 21:44:23 +0200

    items

--- Assets/Scripts/Inventory/TestItemEffect.cs
+++ Assets/Scripts/Inventory/TestItemEffect.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "TestItemEffect", menuName = "Inventory/Effects/Test")]
 public class TestItemEffect : ItemEffectBase
 {
     public override void Apply(ItemData item)
