a2f5b5217eeba6b9b795e28c8e1d01bcd344c627
Fzand <farzanzand86@gmail.com>
2026-05-04 13:53:31 +0200

    NPC setup

---
+++ Assets/Scripts/Managers/NPCManager.cs
@@ -0,0 +1,16 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class NPCManager : Singleton<NPCManager>
+{
+    public Animator DMAnimator;
+    public GameObject DMGameObject;
+
+    [Button]
+    public void TestAnimation()
+    {
+        DMAnimator.SetTrigger("ReachOut");
+    }
+    
+    
+}
