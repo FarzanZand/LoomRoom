51adb7e8d0aa9bf52ad09a76bafa52564ac9c677
Fzand <farzanzand86@gmail.com>
2026-05-24 22:07:57 +0200

    damage flash

--- Assets/Scripts/Player/PlayerFX.cs
+++ Assets/Scripts/Player/PlayerFX.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerFX : CharacterFX
 {
@@ -15,6 +17,15 @@ public class PlayerFX : CharacterFX
     [SerializeField] CinemachineImpulseSource onHitImpulse;
     [SerializeField] CinemachineImpulseSource onHurtImpulse;
 
+    [Header("Hurt Flash")]
+    [SerializeField] bool  enableHurtFlash       = true;
+    [SerializeField] Image hurtFlashImage;
+    [SerializeField] Color hurtFlashColor        = new Color(1f, 0f, 0f, 1f);
+    [SerializeField, Range(0f, 1f)] float hurtFlashPeakAlpha = 0.35f;
+    [SerializeField] float hurtFlashDuration     = 0.3f;
+
+    Coroutine hurtFlashRoutine;
+
     public override void NotifyHitLanded(Vector3 contactPoint)
     {
         base.NotifyHitLanded(contactPoint);
@@ -27,5 +38,33 @@ public class PlayerFX : CharacterFX
     {
         base.NotifyHurtReceived(amount, direction);
         onHurtImpulse?.GenerateImpulse();
+
+        if (enableHurtFlash && hurtFlashImage != null)
+        {
+            if (hurtFlashRoutine != null) StopCoroutine(hurtFlashRoutine);
+            hurtFlashRoutine = StartCoroutine(HurtFlashRoutine());
+        }
+    }
+
+    IEnumerator HurtFlashRoutine()
+    {
+        SetFlashAlpha(hurtFlashPeakAlpha);
+        float elapsed = 0f;
+        while (elapsed < hurtFlashDuration)
+        {
+            elapsed += Time.unscaledDeltaTime;
+            SetFlashAlpha(Mathf.Lerp(hurtFlashPeakAlpha, 0f, elapsed / hurtFlashDuration));
+            yield return null;
+        }
+        SetFlashAlpha(0f);
+        hurtFlashRoutine = null;
+    }
+
+    void SetFlashAlpha(float alpha)
+    {
+        Color c = hurtFlashColor;
+        c.a = alpha;
+        hurtFlashImage.color = c;
+        hurtFlashImage.gameObject.SetActive(alpha > 0f);
     }
 }
